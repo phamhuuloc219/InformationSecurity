baf48cc51a73f6a5c12c3a4491e58f3546eaeb40
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-09-19 11:02:59 +0700

    add translation degree

--- ceasar.py
+++ ceasar.py
@@ -1,12 +1,13 @@
 def  main():
     s = input("Enter your message: ")
+    translation_degree = int(input("Enter the translation degree: "))
     alpha = "abcdefghijklmnopqrstuvwxyz"
     output = ""
     i = 0
     while(i < len(s)):
         ind1 = alpha.index(s[i])
-        ind2 = (ind1 +3 ) %26 +1
-        i+=1
+        ind2 = (ind1 + translation_degree) % 26 + 1
+        i += 1
         x = alpha[ind2]
         output += x
     print(output)
